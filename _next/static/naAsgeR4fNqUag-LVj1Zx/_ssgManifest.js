97f5ddb0389c097b1973e4c1bb1c9aa54fa2aa67
Josh Wang <joshua.wang.h@gmail.com>
2022-11-15 19:06:29 -0500

    update the public url

---
+++ _next/static/naAsgeR4fNqUag-LVj1Zx/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ftags","\u002Fabout","\u002Fsolutions","\u002F","\u002Fsolutions\u002F[id]","\u002Fposts\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
