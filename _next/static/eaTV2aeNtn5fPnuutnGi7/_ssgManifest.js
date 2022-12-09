a09a31b44faa40607218fe71df4289abf907bcb7
Josh Wang <joshua.wang.h@gmail.com>
2022-12-09 11:13:00 -0500

    Revert "updated about experiences and template"
    This reverts commit 4da1a90a43c46ea2c262fc4a5ff8ec608e5014cc.

---
+++ _next/static/eaTV2aeNtn5fPnuutnGi7/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fposts\u002F[id]","\u002Fsolutions","\u002Fsolutions\u002F[id]","\u002Ftags"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
