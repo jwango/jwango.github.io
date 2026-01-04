3af82b9f86b583a6ad5f23fe71f46c6571a9f7ea
jwango <joshua.wang.h@gmail.com>
2026-01-03 22:19:59 -0500

    remove unused _next dir

--- _next/static/PJIaH0lSzXeDa5zx5xrky/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fposts\u002F[id]","\u002Fsolutions","\u002Fsolutions\u002F[id]","\u002Ftags"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
